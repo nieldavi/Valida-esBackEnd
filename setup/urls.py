6134e259df139a8ffa318f76248f3c617725a2d6
Laís Urano <95968249+uranolais@users.noreply.github.com>
2024-03-25 10:41:12 -0300

    aula_01

--- setup/urls.py
+++ setup/urls.py
@@ -1,16 +1,8 @@
 from django.contrib import admin
-from django.urls import path,include
-from escola.views import EstudanteViewSet,CursoViewSet, MatriculaViewSet, ListaMatriculaEstudante,ListaMatriculaCurso
-from rest_framework import routers
-
-router = routers.DefaultRouter()
-router.register('estudantes',EstudanteViewSet,basename='Estudantes')
-router.register('cursos',CursoViewSet,basename='Cursos')
-router.register('matriculas',MatriculaViewSet,basename='Matriculas')
+from django.urls import path
+from escola.views import estudantes
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include(router.urls)),
-    path('estudantes/<int:pk>/matriculas/',ListaMatriculaEstudante.as_view()),
-    path('cursos/<int:pk>/matriculas/',ListaMatriculaCurso.as_view()),
+    path('estudantes/',estudantes),
 ]
