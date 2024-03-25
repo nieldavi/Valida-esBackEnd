d788d74d8db8829d268e76e30861ca41fb527a1a
Laís Urano <95968249+uranolais@users.noreply.github.com>
2024-03-25 11:03:40 -0300

    Adicionando model Matricula e seus recursos

--- escola/serializers.py
+++ escola/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from escola.models import Estudante,Curso
+from escola.models import Estudante,Curso, Matricula
 
 class EstudanteSerializer(serializers.ModelSerializer):
     class Meta:
@@ -11,5 +11,9 @@ class CursoSerializer(serializers.ModelSerializer):
         model = Curso
         fields = '__all__'
 
+class MatriculaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Matricula
+        exclude = []
         
         
